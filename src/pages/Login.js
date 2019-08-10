0c1709145123ff46af697347ea17f9b3ee23ea5c
Roger <12768403+rogix@users.noreply.github.com>
2019-08-09 22:58:40 -0300

    chores: finishe login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+
 import { KeyboardAvoidingView, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 
 import logo from '../assets/logo.png';
+import api from '../services/api';
 
 export default function Login({ navigation }) {
-	function handleLogin() {
-		navigation.navigate('Main');
+	const [user, setUser] = useState('');
+
+	useEffect(() => {
+		AsyncStorage.getItem('user').then(user => {
+			if (user) {
+				navigation.navigate('Main', { user });
+			}
+		});
+	}, []);
+
+	async function handleLogin() {
+		const response = await api.post('/devs', { username: user });
+
+		const { _id } = response.data;
+
+		await AsyncStorage.setItem('user', _id);
+
+		console.log(_id);
+
+		navigation.navigate('Main', { _id });
 	}
 
 	return (
@@ -18,6 +39,8 @@ export default function Login({ navigation }) {
 				placeholder="Digite seu usuário no Github"
 				placeholderTextColor="#999"
 				style={styles.input}
+				value={user}
+				onChangeText={setUser}
 			/>
 
 			<TouchableOpacity onPress={handleLogin} style={styles.button}>
